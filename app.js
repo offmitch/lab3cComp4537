b54a9973efdbc555a4d91d22e57c8a4c836aa62d
Mitchell <mitchell.macdonald10@gmail.com>
2025-09-26 16:45:35 -0700

    Fixed user end responses

--- app.js
+++ app.js
@@ -6,11 +6,27 @@ const messages = require("./lang/messages/en/user.js");
 class Messenger {
 
   getSavedMessage() {
-    return `${messages.savedMessage}`;
+    return messages.savedMessage;
   }
 
    getFailedMessage() {
-    return `${messages.failedMessage}`;
+    return messages.failedMessage;
+  }
+
+  getIntroMessage() {
+    return messages.introMessage;
+  }
+
+  getMissingTextMessage() {
+    return messages.missingTextMessage;
+  }
+
+  getMissingTextMessage() {
+    return messages.missingTextMessage;
+  }
+
+  getErrorReadingFileMessage(file) {
+    return `${messages.errorReadingFileMessage}: ${file}`;
   }
 
 }
@@ -35,20 +51,44 @@ const appender = new FileAdder("file.txt");
 const server = http.createServer((req, res) => {
   const fullUrl = `http://${req.headers.host}${req.url}`;
   const parsedUrl = new URL(fullUrl);
-  
-  // Check the pathname exactly
-  if (parsedUrl.pathname === "/writeFile/") {
-    const text = parsedUrl.searchParams.get("text") || "Text goes here";
+  const messenger = new Messenger();
 
-    // Append the text to file.txt
+  
+  //check pathname exactly
+  if (parsedUrl.pathname === "/writeFile/") {
+    const text = parsedUrl.searchParams.get("text");
+    if (!text) {
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      res.end(messenger.getMissingTextMessage());
+      return;
+    }
     fs.appendFileSync("file.txt", `${text}\n`);
 
-    const messenger = new Messenger();
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end(`${text} : ${messenger.getSavedMessage()}`);
-  } else {
+  } else if (parsedUrl.pathname === "/readFile/") {
+    const file = parsedUrl.searchParams.get("file");
+
+    if (!file) {
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      res.end(messenger.getMissingFileMessage());
+      return;
+    }
+
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(messenger.getErrorReadingFileMessage(file));
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(data);
+    });
+
+    }else{
+    
     res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("didnt find it");
+    res.end(messenger.getIntroMessage());
   }
 });
 
--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -1,6 +1,10 @@
 const messages = {
     savedMessage: "has been saved successfully!",
-    failedMessage: "Error writing to file"
+    failedMessage: "Error writing to file",
+    introMessage: "Type a message to add to the file or read it.",
+    missingTextMessage: "Missing 'text' query parameter",
+    missingFileMessage: "Missing 'file' query parameter",
+    errorReadingFileMessage: "Error reading file: "
 }
 
 module.exports = messages;
